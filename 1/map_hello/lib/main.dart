c630d37e19ba53a3bcdb6602efd762be4b03402c
RicardoQuaresma <bunyamineymen@gmail.com>
2019-08-25 14:28:31 +0300

    1

---
+++ 1/map_hello/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          height: 400.0,
+          width: MediaQuery.of(context).size.width,
+          child: GoogleMap(
+            initialCameraPosition:
+                CameraPosition(target: LatLng(40.7128, -74.0060), zoom: 15.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
